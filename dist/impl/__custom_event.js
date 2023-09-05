23b00dbb5fd4eec5e9d7d632cdce6d9fd162d6cc
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-09-05 15:20:44 -0400

    Add support for `CustomEvent` inside `Worklet`

---
+++ dist/impl/__custom_event.js
@@ -0,0 +1,22 @@
+// Did you know that CustomEvent isn't defined in Worklets? Fun!!
+globalThis.CustomEvent ??= class CustomEvent extends Event {
+    constructor(type, eventInitDict) {
+        super(type, eventInitDict);
+        this.detail = eventInitDict?.detail;
+    }
+    /**
+     * Returns any custom data event was created with. Typically used for synthetic events.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/detail)
+     */
+    detail;
+    /**
+     * @deprecated
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/initCustomEvent)
+     */
+    initCustomEvent(type, bubbles, cancelable, detail) {
+        this.detail = (detail ?? this.detail);
+    }
+};
+export {};
---
+++ src/impl/__custom_event.ts
@@ -0,0 +1,26 @@
+
+// Did you know that CustomEvent isn't defined in Worklets? Fun!!
+globalThis.CustomEvent ??= class CustomEvent<T = any> extends Event {
+
+    constructor(type: string, eventInitDict?: CustomEventInit<T>) {
+        super(type, eventInitDict);
+        this.detail = eventInitDict?.detail!;
+    }
+
+    /**
+     * Returns any custom data event was created with. Typically used for synthetic events.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/detail)
+     */
+    detail: T;
+
+    /**
+     * @deprecated
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/initCustomEvent)
+     */
+    initCustomEvent(type: string, bubbles?: boolean, cancelable?: boolean, detail?: T): void { 
+        this.detail = (detail ?? this.detail)!;
+    }
+
+}
